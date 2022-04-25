b6e8b1546894762bda04ce6c4f252b833e5b3d12
Aditya Jain <aditya151101@gmail.com>
2022-04-25 20:37:47 +0530

    popular api added

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Popular() {
+  const [popular, setPopular] = useState([]);
 
-    
+  useEffect(() => {
+    getPopular();
+  }, []);
 
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+    );
+    const data = await api.json();
+    console.log(data.recipes);
+
+    setPopular(data.recipes);
+  };
+
+  const popularDishes = popular.map((recipe) => {
     return (
-        <div>Popular</div>
-  )
+      <div className="popular" key={recipe.id}>
+        <div className="popCard">
+          <p>{recipe.title}</p>
+          <img src={recipe.image} alt="{recipe.title}"></img>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div>
+      <h3>Popular Picks:</h3>
+      {popularDishes}
+    </div>
+  );
 }
