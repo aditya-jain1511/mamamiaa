f0be33191840b0609ccc3e3a0894448c45a0e565
Aditya Jain <aditya151101@gmail.com>
2022-04-26 14:14:09 +0530

    Added popular and css styled it- added local storage options for api fetched data

--- src/Components/Popular.jsx
+++ src/Components/Popular.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import '@splidejs/splide/dist/css/splide.min.css';
 
 export default function Popular() {
   const [popular, setPopular] = useState([]);
@@ -8,30 +10,51 @@ export default function Popular() {
   }, []);
 
   const getPopular = async () => {
-    const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
-    );
-    const data = await api.json();
-    console.log(data.recipes);
+    //adding fetched api call to local storage so we dont have to fetch it again and again from spoonacular if already present in browser cache
 
-    setPopular(data.recipes);
+    // checking for items in local storage
+    const check = localStorage.getItem('popular')
+    if (check){
+      setPopular(JSON.parse(check))
+    }
+    else{
+      const api = await fetch( `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+      );
+
+      const data = await api.json();
+      //adding fetched api item to local storage of browser 
+      localStorage.setItem('popular',JSON.stringify(data.recipes))
+      setPopular(data.recipes);
+      console.log(data.recipes);    
+    }
   };
 
   const popularDishes = popular.map((recipe) => {
     return (
-      <div className="popular" key={recipe.id}>
-        <div className="popCard">
-          <p>{recipe.title}</p>
-          <img src={recipe.image} alt="{recipe.title}"></img>
-        </div>
-      </div>
+      <SplideSlide key={recipe.id}>
+        
+          <div className="popCard">
+            <p className="popTitle">{recipe.title}</p>
+            <img className="popImg" src={recipe.image} alt="{recipe.title}"></img>
+            <div className="gradient"></div>
+          </div>
+        
+      </SplideSlide>
     );
   });
 
   return (
-    <div>
+    <div className="popular" >
       <h3>Popular Picks:</h3>
-      {popularDishes}
+      <Splide options={{
+        perPage: 4,
+        arrows: false,
+        pagination: false,
+        drag: 'free',
+        gap: '2rem'
+      }}>
+        {popularDishes}
+      </Splide>
     </div>
   );
 }
