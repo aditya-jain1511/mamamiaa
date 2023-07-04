bde62e87a1fda3f9b8f81cce3cbdad2b8fde3898
Aditya Jain <73277210+aditya-jain1511@users.noreply.github.com>
2023-07-04 15:28:10 +0530

    Update Eggie.jsx

--- src/Components/Eggie.jsx
+++ src/Components/Eggie.jsx
@@ -20,7 +20,7 @@ export default function Eggie() {
       setEggie(JSON.parse(check));
     } else {
       const api = await fetch(
-        `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=10&tags=vegetarian`
+        `https://api.spoonacular.com/recipes/random?apiKey=642b89fe5cf64cb7b5da7a6c57b6eab4&number=10&tags=vegetarian`
       );
 
       const data = await api.json();
